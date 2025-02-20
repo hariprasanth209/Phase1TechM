e3224a10ce787e1b022d24b8b93ec33863028371
Hariprasanthkiot <2k21it22@kiot.ac.in>
2025-02-20 10:14:57 +0530

    Updated files and added new files

--- Assets/JS/ShowDetails.js
+++ Assets/JS/ShowDetails.js
@@ -2,4 +2,9 @@
     document.getElementById("planPrice").innerText = localStorage.getItem("planPrice");
     document.getElementById("planData").innerText = localStorage.getItem("planData");
     document.getElementById("validDays").innerText = localStorage.getItem("validDays");
+    const loggedInMobile = localStorage.getItem("loggedInMobile");
+    const guestMobile = localStorage.getItem("guestMobile");
+
+    const mobileToDisplay = guestMobile || loggedInMobile;
+    document.getElementById("phone").innerText = mobileToDisplay;
   };
